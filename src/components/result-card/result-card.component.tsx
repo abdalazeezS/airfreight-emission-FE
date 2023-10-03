7321f02a87b6cff63fd85444a7c54c23798444a9
abdalazeez <abdalazeez@aeliasoft.com>
2023-10-03 09:46:00 +0300

    adding categories and styles for emission levels

--- src/components/result-card/result-card.component.tsx
+++ src/components/result-card/result-card.component.tsx
@@ -1,7 +1,15 @@
 import './result-card.css';
 
 const ResultCard = ({ trip }: any) => {
-
+  var category = '';
+  if (trip.emission_level < 35) {
+    category = 'green';
+  } else if (trip.emission_level < 80) {
+    category = 'yellow';
+  } else {
+    category = 'red'
+  }
+  console.log(category);
   return (
     <div className="search-result-card">
       <div className="trip-details">
@@ -12,11 +20,11 @@ const ResultCard = ({ trip }: any) => {
         <p><strong>Airline:</strong> {trip.airline}</p>
         <p><strong>Date:</strong> {trip.date}</p>
       </div>
-      <div className="emission-level">
+      <div className={`emission-level ${category}`}>
         <p><strong>Airfreight Emission level:</strong> <br />
           {trip.emission_level} kg CO2</p>
       </div>
-    </div>
+    </div >
   )
 }
 
