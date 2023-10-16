8d14bb4d0c300a72a493b107e8b746aee8cbceb6
abdalazeez <abdalazeez@aeliasoft.com>
2023-10-16 16:42:01 +0300

    Revert "add initial design system folder"
    This reverts commit 4c5ca41a5d4ccd090891ca473dfdd3b3396a20c5.

--- src/design-system/button/button.stories.tsx
+++
@@ -1,7 +0,0 @@
-import CustomizedButton from "./button"
-export default {
-  title: 'CustomizedButton',
-  component: CustomizedButton
-}
-
-export const Primary = () => <CustomizedButton variant='primary'>primary</CustomizedButton>
--- src/design-system/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as CustomizedButton } from './button/button';
-export { default as CheckBoxListItem } from './checkbox-list-item/checkbox-item';
-export { default as ListItem } from './list-item/list-item';
